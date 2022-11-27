cf5de20264f8bd65d92094b91615e56e525ca18b
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 14:56:10 +0300

    check: synthetic data

--- src/components/Event/EventLable.jsx
+++ src/components/Event/EventLable.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const EventLable = ({event, className}) => {
 
+    function DateFormat(date){
+        date = new Date(Date(date));
+        return date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear() % 100;
+    }
+
     return (
         <div className={'item ' + className}>
             <span className={'title'}>{event.name}</span>
-            <span className={'date'}>{event.date}</span>
+            <span className={'date'}>{DateFormat(event.date)}</span>
             <span className={'add-btn'}>
                 Назад
             </span>
