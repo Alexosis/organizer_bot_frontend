f69455fff571754b17b2db661c05345d43e0ddba
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 11:46:53 +0300

    test

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-const Event = ({event, className, onBack}) => {
-    const onBackHandler = () => {
-        onBack(product);
-    }
-
-    return (
-        <div className={'product ' + className}>
-            <div className={'title'}>{event.name}</div>
-            <div className={'date'}>{event.date}</div>
-            <div className={'description'}>{event?.description}</div>
-            <div className={'priority'}>{event?.description}</div>
-            <button className={'add-btn'} onClick={onBackHandler}>
-                Назад
-            </button>
-        </div>
-    );
-};
+// const Event = ({event, className, onBack}) => {
+//     const onBackHandler = () => {
+//         onBack(product);
+//     }
+//
+//     return (
+//         <div className={'product ' + className}>
+//             <div className={'title'}>{event.name}</div>
+//             <div className={'date'}>{event.date}</div>
+//             <div className={'description'}>{event?.description}</div>
+//             <div className={'priority'}>{event?.description}</div>
+//             <button className={'add-btn'} onClick={onBackHandler}>
+//                 Назад
+//             </button>
+//         </div>
+//     );
+// };
 
 export default Event;
--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import Event from "../Event/Event";
+// import Event from "../Event/Event";
 
-const EventList = () => {
-    // const data = {
-    //     fet
-    // }
-
-    return (
-        <div className={'list'}>
-            {events.map(event => (
-                <Event
-                    event = {event}
-                    className = {'event'}
-                    onBack = {onBack}
-                />
-            ))}
-        </div>
-    );
-};
+// const EventList = () => {
+//     // const data = {
+//     //     fet
+//     // }
+//
+//     return (
+//         <div className={'list'}>
+//             {events.map(event => (
+//                 <Event
+//                     event = {event}
+//                     className = {'event'}
+//                     onBack = {onBack}
+//                 />
+//             ))}
+//         </div>
+//     );
+// };
 
 export default EventList;
