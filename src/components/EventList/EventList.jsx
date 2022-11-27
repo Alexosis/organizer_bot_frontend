4300e1a657f53c59d95edf65d83388b5acc6c09c
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 13:01:08 +0300

    add: event list

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
+import Event from "../Event/Event";
+import {useTelegram} from "../hook/useTelegram";
+
+const {user} = useTelegram()
+let events = await fetch(`http://127.0.0.1:8000/api/todos/get/${user?.id}`);
 
 const EventList = () => {
+
     return (
-        <div>
-            
+        <div className={'list'}>
+            {events.map(event => (
+                <Event
+                    event = {event}
+                    className = {'event'}
+                    onBack = {onBack}
+                />
+            ))}
         </div>
     );
 };
 
 export default EventList;
-
-// import React from 'react';
-// import Event from "../Event/Event";
-//
-// const EventList = () => {
-//     const data = {
-//         fet
-//     }
-//
-//     return (
-//         <div className={'list'}>
-//             {events.map(event => (
-//                 <Event
-//                     event = {event}
-//                     className = {'event'}
-//                     onBack = {onBack}
-//                 />
-//             ))}
-//         </div>
-//     );
-// };
-//
-// export default EventList;
