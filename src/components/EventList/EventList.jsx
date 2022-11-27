f0b22cd9976cef9451b794ceedad52d33b01c321
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 18:41:23 +0300

    check: interface

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -3,22 +3,25 @@ import React from 'react';
 // import header from "../header/Header";
 import './EventList.css'
 import EventLable from "../Event/EventLable";
+import db from'../../../public/db.json'
 
-const events_synt = {
-    "todos":[
-        {
-            "name": "Test1",
-            "description": "test desc 1",
-            "date": "2022-11-27T12:41:00+3:00",
-            "userID": 422260714
-        },
-        {
-            "name": "Test2",
-            "description": "test desc 2",
-            "date": "2022-12-27T12:41:00+3:00",
-            "userID": 422260714
-        }
-    ]}
+// const events_synt = {
+//     "todos":[
+//         {
+//             "name": "Test1",
+//             "description": "test desc 1",
+//             "date": "2022-11-27T12:41:00+3:00",
+//             "userID": 422260714
+//         },
+//         {
+//             "name": "Test2",
+//             "description": "test desc 2",
+//             "date": "2022-12-27T12:41:00+3:00",
+//             "userID": 422260714
+//         }
+//     ]}
+
+const events_synt = db;
 
 const EventList = () => {
 
@@ -32,7 +35,7 @@ const EventList = () => {
             {/*{response}*/}
             {/*{events}*/}
             {events_synt.todos.map(event => (
-                <EventLable className={"event"} event={event} />
+                <EventLable className={"event"} event={event.fields} />
             ))}
         </div>
     );
