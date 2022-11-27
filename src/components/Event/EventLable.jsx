91d742603e0fcb8c37d5057abd38fd11a55dfe64
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 18:29:29 +0300

    check: interface

--- src/components/Event/EventLable.jsx
+++ src/components/Event/EventLable.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import './Event.css';
-import Collapsible from "react-collapsible";
 
 const EventLable = ({event}) => {
 
-    // const {getCollapseProps, getToggleProps, isExpanded} = useCollapse();
-
-
-    // function DateFormat(date) {
-    //     date = new Date(Date(date));
-    //     return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() % 100;
-    // }
+    function DateFormat(date) {
+        date = new Date(Date(date));
+        return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() % 100;
+    }
 
     // function expand() {
     //     let content = document.getElementsByClassName("expand")[0];
@@ -36,11 +32,13 @@ const EventLable = ({event}) => {
 
     return (
         <div className={"item"}>
-            <Collapsible trigger={event.name} >
-                <div className={"expand"}>
-                    <div className={'description'}>{event.description}</div>
-                </div>
-            </Collapsible>
+            <table>
+                <tr>
+                <td className={'name'}>{event.name}</td>
+                <td className={'date'}>{DateFormat(event.date)}</td>
+                </tr>
+            </table>
+            <div className={'description'}>{event.description}</div>
         </div>);
 
 
