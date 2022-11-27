5870bac06131181964d113ca91aecd6aba1cc365
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 14:22:58 +0300

    check: synthetic data

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,33 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Event = () => {
+const Event = ({event, className}) => {
+
     return (
-        <div>
-            
+        <div className={'product ' + className}>
+            <div className={'title'}>{event.name}</div>
+            <div className={'date'}>{event.date}</div>
+            <div className={'description'}>{event?.description}</div>
+            <div className={'priority'}>{event?.description}</div>
+            <button className={'add-btn'}>
+                Назад
+            </button>
         </div>
     );
 };
 
 export default Event;
-
-// import React from "react";
-//
-// const Event = ({event, className, onBack}) => {
-//     const onBackHandler = () => {
-//         onBack(product);
-//     }
-//
-//     return (
-//         <div className={'product ' + className}>
-//             <div className={'title'}>{event.name}</div>
-//             <div className={'date'}>{event.date}</div>
-//             <div className={'description'}>{event?.description}</div>
-//             <div className={'priority'}>{event?.description}</div>
-//             <button className={'add-btn'} onClick={onBackHandler}>
-//                 Назад
-//             </button>
-//         </div>
-//     );
-// };
-//
-// export default Event;
--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 // import Event from "../Event/Event";
-import {useTelegram} from "../hook/useTelegram";
+// import {useTelegram} from "../hook/useTelegram";
+// import header from "../header/Header";
+import Event from "../Event/Event";
 
 const EventList = async () => {
 
-    const {user} = useTelegram()
-    let response = await fetch(`http://127.0.0.1:8000/api/todos/get/${user?.id}`);
-    let events = await response.json();
+    // const {user} = useTelegram()
+    // let response = await fetch(`http://127.0.0.1:8000/api/todos/get/${user?.id}`, {headers:{Access-Control-Allow-Origin: *}});
+    // let events = await response.json();
+    const events = {
+        "todos":[
+        {
+            "name": "Test1",
+            "description": "test desc 1",
+            "date": "2022-11-27T12:41:00+3:00",
+            "userID": 422260714
+        },
+        {
+            "name": "Test2",
+            "description": "test desc 2",
+            "date": "2022-12-27T12:41:00+3:00",
+            "userID": 422260714
+        }
+    ]}
 
     return (
         <div className={'list'}>
-            {events}
-            {/*{events.todos.map(event => (*/}
-            {/*    event.name*/}
-            {/*))}*/}
+            {events.todos.map(event => (
+                <Event className={"event"} event={event} />
+            ))}
             check!
         </div>
     );
