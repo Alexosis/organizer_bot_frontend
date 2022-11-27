4c7cf1bd0ba6c96cf58697de62554a1afcb30cc6
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 17:29:38 +0300

    check: interface

--- src/components/Event/EventLable.jsx
+++ src/components/Event/EventLable.jsx
@@ -1,35 +1,49 @@
 import React from "react";
 import './Event.css';
+import Collapsible from "react-collapsible";
 
-const EventLable = ({event, className}) => {
+const EventLable = ({event}) => {
 
-    function DateFormat(date){
-        date = new Date(Date(date));
-        return date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear() % 100;
-    }
+    // const {getCollapseProps, getToggleProps, isExpanded} = useCollapse();
 
-    function expand(){
-        let content = document.getElementsByClassName("expand")[0];
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    }
+
+    // function DateFormat(date) {
+    //     date = new Date(Date(date));
+    //     return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() % 100;
+    // }
+
+    // function expand() {
+    //     let content = document.getElementsByClassName("expand")[0];
+    //     if (content.style.display === "block") {
+    //         content.style.display = "none";
+    //     } else {
+    //         content.style.display = "block";
+    //     }
+    // }
+
+    // <div className={'item ' + className}>
+    {/*<table onClick={expand}>*/}
+    {/*    <tr>*/}
+    {/*        <td className={'title'}>{event.name}</td>*/}
+    {/*        <td className={'date'}>{DateFormat(event.date)}</td>*/}
+    {/*    </tr>*/}
+    {/*</table>*/}
+    {/*    <div className={"expand"}>*/}
+    {/*        <div className={'description'}>{event.description}</div>*/}
+    {/*    </div>*/}
+    {/*</div>*/}
+
 
     return (
-        <div className={'item ' + className}>
-            <table onClick={expand}>
-                <tr>
-                    <td className={'title'}>{event.name}</td>
-                    <td className={'date'}>{DateFormat(event.date)}</td>
-                </tr>
-            </table>
+        <Collapsible trigger="here">
             <div className={"expand"}>
                 <div className={'description'}>{event.description}</div>
             </div>
-        </div>
-    );
+        </Collapsible>    );
+
+
+
+
 };
 
 export default EventLable;
--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import {useTelegram} from "../hook/useTelegram";
 // import header from "../header/Header";
 import './EventList.css'
-// import EventLable from "../Event/EventLable";
+import EventLable from "../Event/EventLable";
 
-// const events = {
-//     "todos":[
-//         {
-//             "name": "Test1",
-//             "description": "test desc 1",
-//             "date": "2022-11-27T12:41:00+3:00",
-//             "userID": 422260714
-//         },
-//         {
-//             "name": "Test2",
-//             "description": "test desc 2",
-//             "date": "2022-12-27T12:41:00+3:00",
-//             "userID": 422260714
-//         }
-//     ]}
+const events_synt = {
+    "todos":[
+        {
+            "name": "Test1",
+            "description": "test desc 1",
+            "date": "2022-11-27T12:41:00+3:00",
+            "userID": 422260714
+        },
+        {
+            "name": "Test2",
+            "description": "test desc 2",
+            "date": "2022-12-27T12:41:00+3:00",
+            "userID": 422260714
+        }
+    ]}
 
 const EventList = () => {
 
@@ -31,9 +31,9 @@ const EventList = () => {
         <div className={'list'}>
             {response}
             {events}
-            {/*{events.todos.map(event => (*/}
-            {/*    <EventLable className={"event"} event={event} />*/}
-            {/*))}*/}
+            {events_synt.todos.map(event => (
+                <EventLable className={"event"} event={event} />
+            ))}
         </div>
     );
 };
