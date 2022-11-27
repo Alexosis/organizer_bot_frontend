751527821ef85c6f23bf10681c2a09689f07eb1f
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 16:20:11 +0300

    add: expand list

--- src/components/Event/EventLable.jsx
+++ src/components/Event/EventLable.jsx
@@ -8,16 +8,25 @@ const EventLable = ({event, className}) => {
         return date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear() % 100;
     }
 
+    function expand(){
+        let content = document.getElementsByClassName("expand")[0];
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    }
+
     return (
         <div className={'item ' + className}>
-            <table>
+            <table onClick={expand}>
                 <tr>
                     <td className={'title'}>{event.name}</td>
                     <td className={'date'}>{DateFormat(event.date)}</td>
                 </tr>
             </table>
             <div className={"expand"}>
-                <div className={'title'}></div>
+                <div className={'description'}>{event.description}</div>
             </div>
         </div>
     );
--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import {useTelegram} from "../hook/useTelegram";
+// import {useTelegram} from "../hook/useTelegram";
 // import header from "../header/Header";
 import './EventList.css'
-// import EventLable from "../Event/EventLable";
+import EventLable from "../Event/EventLable";
 
-// const events = {
-//     "todos":[
-//         {
-//             "name": "Test1",
-//             "description": "test desc 1",
-//             "date": "2022-11-27T12:41:00+3:00",
-//             "userID": 422260714
-//         },
-//         {
-//             "name": "Test2",
-//             "description": "test desc 2",
-//             "date": "2022-12-27T12:41:00+3:00",
-//             "userID": 422260714
-//         }
-//     ]}
+const events = {
+    "todos":[
+        {
+            "name": "Test1",
+            "description": "test desc 1",
+            "date": "2022-11-27T12:41:00+3:00",
+            "userID": 422260714
+        },
+        {
+            "name": "Test2",
+            "description": "test desc 2",
+            "date": "2022-12-27T12:41:00+3:00",
+            "userID": 422260714
+        }
+    ]}
 
 const EventList = () => {
 
-    const {user} = useTelegram()
-    let response = fetch(`https://127.0.0.1:8000/api/todos/get/${user?.id}`, {mode:'no-cors'});
-    // let events = response.json();
+    // const {user} = useTelegram()
+    // let response = fetch(`https://127.0.0.1:8000/api/todos/get/${user?.id}`, {mode:'no-cors'});
+    // // let events = response.json();
 
 
     return (
         <div className={'list'}>
-            {response}
-            {/*{events.todos.map(event => (*/}
-            {/*    <EventLable className={"event"} event={event} />*/}
-            {/*))}*/}
+            {/*{response}*/}
+            {events.todos.map(event => (
+                <EventLable className={"event"} event={event} />
+            ))}
         </div>
     );
 };
