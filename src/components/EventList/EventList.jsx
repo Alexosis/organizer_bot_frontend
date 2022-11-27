ddaf4dbb2b6721c239aaed5369ad1aa079d70343
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2022-11-27 13:15:13 +0300

    add: event list response

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -5,7 +5,7 @@ import {useTelegram} from "../hook/useTelegram";
 const EventList = async () => {
 
     const {user} = useTelegram()
-    let events = await fetch(`http://127.0.0.1:8000/api/todos/get/${user?.id}`);
+    let events = (await fetch(`http://127.0.0.1:8000/api/todos/get/${user?.id}`)).json();
 
     return (
         <div className={'list'}>
